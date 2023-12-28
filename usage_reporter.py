7f3a6368b9c2a987a81002afd1163241a4dea18a
Pete Bentley <prb@google.com>
2023-12-28 18:27:44 +0000

    Rework parser state machine.
    Now uses a class with a lambda pointing at the current 'state'
    method in order to avoid requiring Python 3.10 for its 'match'
    statement.

--- usage_reporter.py
+++ usage_reporter.py
@@ -39,6 +39,10 @@ from typing import Optional
 # If an 'update' argument is given this script will attempt to update the data in index.md.  It's
 # a pretty crude parser which relies on the data for each Android version being inserted
 # in the table row for a particular API level, see level_to_version dict below.
+#
+# MacOS Note:  Should work with the python3 that comes with the Xcode command line tools, but
+# you'll need to install the 'requests' module, for example:
+#      python3 -m pip install requests
 
 versions_to_report_on = [
     '14', '13', '12', '11', '10', '9',
@@ -120,56 +124,98 @@ def accumulate(input_values: dict[str, float]) -> dict[str, float]:
     return values
 
 
-def update_index(values: dict[str, float]):
-    infilename = 'index.md'
-    tmpfilename = 'index.tmp'
-    level_re = re.compile('.*Level (\\d+)')
-    data_re = re.compile('(.*progress-cell.html.* percentage=)([0-9.]+)(.*)')
-    state = 'init'
-    api_level = None
-    with open(infilename, mode='r', encoding="utf-8") as infile:
-        with open(tmpfilename, mode='w', encoding="utf-8") as outfile:
-            for line in infile:
-                match state:
-                    case 'init':
-                        if '<table' in line:
-                            state = 'table'
-                    case 'table':
-                        if '<tr' in line:
-                            state = 'row'
-                        if '</table>' in line:
-                            state = 'done'
-                    case 'row':
-                        if '</tr>' in line:
-                            state = 'table'
-                            api_level = None
-                        match = re.match(level_re, line)
-                        if match:
-                            if api_level:
-                                raise AssertionError('Parse error')
-                            api_level = int(match[1])
-                        match = re.match(data_re, line)
-                        if match:
-                            if not api_level:
-                                raise AssertionError('Parse error')
-                            if api_level in level_to_version:
-                                value = values[level_to_version[api_level]]
-                                newline = "{}{:.01f}{}\n".format(match[1], value, match[3])
-                                outfile.write(newline)
-                                api_level = None
-                                continue
-                    case 'done':
-                        pass
-                    case _:
-                        raise AssertionError('Invalid state')
-                outfile.write(line)
-    os.rename(tmpfilename, infilename)
+class ParseError(AssertionError):
+    pass
+
+
+class IndexUpdater:
+    def __init__(self):
+        self.values = None
+        self.in_filename = 'index.md'
+        self.tmp_filename = 'index.tmp'
+        # Matches and extracts a level number from anywhere in a line of text.
+        self.level_re = re.compile('.*Level (\\d+)')
+        # Matches a line with a progressive-cell and extracts the current value.
+        self.data_re = re.compile('(.*progress-cell.html.* percentage=)([0-9.]+)(.*)')
+        # Matches and extracts the "last updated" date.
+        self.updated_re = re.compile(
+            '(\\s+<p>Cumulative.*updated on <b>)(\\w+ \\d+, \\d+)(</b> using.*)')
+        # Current 'state', which is a lambda pointing to the method to use to process the next line.
+        self.process = lambda v: self.init(v)
+        # Last API level found which has not yet been used, else None.
+        self.api_level = None
+        # True once all substitutions have been made.
+        self.finished = False
+        self.seen_api_levels = set()
+
+    def update_index(self, values: dict[str, float]):
+        self.values = values
+        with open(self.in_filename, mode='r', encoding="utf-8") as infile:
+            with open(self.tmp_filename, mode='w', encoding="utf-8") as self.outfile:
+                for line in infile:
+                    self.outfile.write(self.process(line))
+        if not self.finished:
+            raise ParseError('Failed to find all values')
+        for level in level_to_version.keys():
+            if level not in self.seen_api_levels:
+                raise ParseError('Did not update data for API level' + level)
+        os.rename(self.tmp_filename, self.in_filename)
+
+    # Initial state: looking for the first table in the file.
+    def init(self, line: str) -> str:
+        if '<table ' in line:
+            self.process = lambda v: self.table(v)
+        return line
+
+    # State while processing the first table, but not currently within a table row.
+    def table(self, line: str) -> str:
+        if '<tr>' in line:
+            self.process = lambda v: self.table_row(v)
+        if '</table>' in line:
+            self.process = lambda v: self.update_date(v)
+        return line
+
+    # State while processing a table row.
+    def table_row(self, line: str) -> str:
+        match = re.match(self.data_re, line)
+        if match:
+            if not self.api_level or self.api_level not in level_to_version:
+                raise ParseError('API level unknown')
+            new_value = self.values[level_to_version[self.api_level]]
+            self.api_level = None
+            return "{}{:.01f}{}\n".format(match[1], new_value, match[3])
+        else:
+            match = re.match(self.level_re, line)
+            if match:
+                if self.api_level:
+                    raise ParseError('Multiple API levels found')
+                self.api_level = int(match[1])
+                self.seen_api_levels.add(self.api_level)
+            elif '</tr>' in line:
+                self.process = lambda v: self.table(v)
+                self.api_level = None
+        return line
+
+    # State reached after all usage has been updated and we are looking for the update date
+    def update_date(self, line: str) -> str:
+        match = re.match(self.updated_re, line)
+        if match:
+            self.process = lambda v: self.done(v)
+            return ("{}{}{}\n"
+                    .format(match[1], datetime.now().strftime("%B %d, %Y"), match[3]))
+        return line
+
+    # State reached after all substitutions have been performed.
+    def done(self, line: str) -> str:
+        self.finished = True
+        return line
 
 
 def report_and_maybe_update(do_update: bool):
     values = accumulate(download(generate_stats_url()))
     if do_update:
-        update_index(values)
+        updater = IndexUpdater()
+        updater.update_index(values)
     for key in versions_to_report_on:
         print("Android {}: {:.02f}".format(key, values[key]))
 
